23baf168fef968259735246f68f7f94e97e55919
flypx20 <1728925053@qq.com>
2020-09-14 16:42:09 +0800

    第一个typeScript程序

---
+++ greeter.js
@@ -0,0 +1,8 @@
+function greeter(person) {
+    return 'hello,' + person;
+}
+var user = 'huanxiren0';
+var user1 = 0;
+// document.body.innerHTML = greeter(user);
+console.log(greeter(user));
+console.log(greeter(user1));
---
+++ greeter.ts
@@ -0,0 +1,8 @@
+function greeter(person:string){
+    return 'hello,'+person;
+}
+let user = 'huanxiren0';
+let user1 = 0
+// document.body.innerHTML = greeter(user);
+console.log(greeter(user))
+console.log(greeter(user1))
